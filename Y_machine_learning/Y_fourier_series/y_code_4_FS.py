d8b5258b2e8191e2a65fbb6cee08238b3b34b00a
Akaalis-code <akaalis.y@gmail.com>
2024-05-05 23:37:48 +0530

    y_coefficient_calculation_working

--- Y_machine_learning/Y_fourier_series/y_code_4_FS.py
+++ Y_machine_learning/Y_fourier_series/y_code_4_FS.py
@@ -11,6 +11,18 @@ var_lst_an = ['NA'] # list of Cosine weightages
 var_lst_bn = ['NA'] # list of Sine weightages
 
 
+def mthd_get_coef(para_the_func , para_n ,para_an_or_bn):
+    if para_n == 0 and para_an_or_bn == 'an':
+        a0 = (0.3) * ((integrate.quad(lambda x:para_the_func(x) , -(math.pi) , (math.pi)))[0])
+        return a0
+    if para_n != 0 and para_an_or_bn == 'an':    
+        an = (0.3) * ((integrate.quad((lambda x:para_the_func(x) * math.cos(para_n * x)) , -math.pi , math.pi))[0])
+        return an
+    if para_n != 0 and para_an_or_bn == 'bn':    
+        bn = (0.3) * ((integrate.quad((lambda x:para_the_func(x) * math.sin(para_n * x)) , -math.pi , math.pi))[0])
+        return bn
+    return 'NA'
+
 def mthd_output(para_n , para_a0 , para_lst_an , para_lst_bn , para_axis_X):
     f_of_x = (a0)*(1/2) 
     for cntr in range(1 , len(para_lst_an)+1) :
@@ -19,23 +31,12 @@ def mthd_output(para_n , para_a0 , para_lst_an , para_lst_bn , para_axis_X):
     for cntr in range(1 , len(para_lst_bn)+1) :
         f_of_x = f_of_x + para_lst_bn[cntr] * (math.sin( (cntr) * (para_axis_X) ))
 
-    return f_of_x
-
-def mthd_get_coef(para_the_func , para_n ,para_an_or_bn):
-    if para_n == 0 and para_an_or_bn == 'an':
-        a0 = (1/(math.pi)) * (integrate.quad(para_the_func , -(math.pi) , (math.pi)))
-        return a0
-    if para_n != 0 and para_an_or_bn == 'an':    
-        an = (1/(math.pi)) * (integrate.quad((lambda x:para_the_func(x) * math.cos(para_n * x)) , -math.pi , math.pi))
-        return an
-    if para_n != 0 and para_an_or_bn == 'bn':    
-        bn = (1/(math.pi)) * (integrate.quad((lambda x:para_the_func(x) * math.sin(para_n * x)) , -math.pi , math.pi))
-        return bn
-
-var_the_func = ?
+var_the_func = math.sin
 for i in range (0,var_n) :
     if i == 0 :
-        var_a0 = mthd_get_coef(var_the_func , i)
+        var_a0 = mthd_get_coef(var_the_func , i , 'an')
     else :
-        var_lst_an = var_lst_an.append(mthd_get_coef(var_the_func , i , 'an'))
-        var_lst_an = var_lst_an.append(mthd_get_coef(var_the_func , i , 'bn'))
+        var_lst_an.append(mthd_get_coef(var_the_func , i , 'an'))
+        var_lst_bn.append(mthd_get_coef(var_the_func , i , 'bn'))
+
+print(var_a0 , var_lst_an , var_lst_bn)
