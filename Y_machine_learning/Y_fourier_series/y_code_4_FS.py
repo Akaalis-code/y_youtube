707e0ece99c740ce81ce03e472f9b1295575f9f2
Akaalis-code <akaalis.y@gmail.com>
2024-05-11 19:28:55 +0530

    y_swtooth_fourier_completed

--- Y_machine_learning/Y_fourier_series/y_code_4_FS.py
+++ Y_machine_learning/Y_fourier_series/y_code_4_FS.py
@@ -13,13 +13,22 @@ var_fig = pplt.figure()
 var_figMgr = pplt.get_current_fig_manager()
 var_figMgr.full_screen_toggle()
 
-gs = var_fig.add_gridspec(2,2)
+gs = var_fig.add_gridspec(2,3)
 Axes1 = var_fig.add_subplot(gs[0, :])
 Axes2 = var_fig.add_subplot(gs[1, 0])
-Axes3 = var_fig.add_subplot(gs[1, 1])
+Axes4 = var_fig.add_subplot(gs[1, 1])
+Axes3 = var_fig.add_subplot(gs[1, 2])
 
 
-var_axis_X = numpy.linspace(0, 6.28, num=200)
+Axes2.set_xticklabels([])
+
+Axes3.set_xticklabels([])
+
+Axes4.set_xticklabels([])
+Axes4.set_yticklabels([])
+
+
+var_axis_X = numpy.linspace(0, 6.28, num=1000)
 var_n = 100 # number of waves used for construction
 
 var_a0 = 0 # a0 value
@@ -65,24 +74,42 @@ def mthd_output(para_n , para_a0 , para_lst_an , para_lst_bn , para_axis_X):
 
 
 def mth_plot_graph (para_axis_x , para_main_func , para_generated_func , para_cntr):
+    Axes1.cla()
     Axes1.plot(para_axis_x,para_main_func )
     Axes1.plot(para_axis_x,para_generated_func)
-    Axes1.text(0.5,-0.5,'Fourier series output')
-    Axes1.text(0,-0.75,' a0 + sigma({an * COS(n*x)+bn * SIN(n*x)} , range-> 1 , Infinity)' , fontsize =  'small')
+    Axes1.text(3.16,1,'Fourier series output')
+    Axes1.text(3.16,0.75,' a0 + sigma({an * COS(n*x)+bn * SIN(n*x)} , range-> 1 , Infinity)' , fontsize =  'small')
     #mth_sin_and_cosine_plot(var_a0 , var_lst_an , var_lst_bn , para_cntr)
     if para_cntr == 0 :
         Axes2.plot(para_axis_x , var_a0)
     else :
         Axes2.plot(para_axis_x , var_lst_an[para_cntr] * numpy.cos( para_cntr * para_axis_x))
+        Axes2.text(1,-0.04,'an * COS(n*x)')
+
         Axes3.plot(para_axis_x , var_lst_bn[para_cntr] * numpy.sin( para_cntr * para_axis_x))
-        Axes2.text(0.5,-0.02,'an * COS(n*x)')
-        Axes3.text(0.5,-0.75,'bn * SIN(n*x)')
+        Axes3.text(1,-0.75,'bn * SIN(n*x)')
+
+        Axes4.cla()
+        Axes4.set_xticklabels([])
+        Axes4.set_yticklabels([])
+        Axes4.text(0,0.8,'a0 = {0}'.format(var_a0))
+        def mthd_my_rounding_up(para_val) :
+            if round(para_val , 5) == 0 :
+                return 0
+            else :
+                return round(para_val , 5)
+        Axes4.text(0,0.6,'an * COS(n*x) = {0} * COS({1}*x)'.format(mthd_my_rounding_up(var_lst_an[para_cntr]) , para_cntr) , fontsize =  'small' )
+        Axes4.text(0,0.4,'bn * SIN(n*x) = {0} * SIN({1}*x)'.format(mthd_my_rounding_up(var_lst_bn[para_cntr]) , para_cntr) , fontsize =  'small' )
 
 
 
 
+def mthd_input_func(para_x):
+    return signal.sawtooth((2 * numpy.pi * para_x)/3.14)
+var_the_func = mthd_input_func
 
-var_the_func = signal.square
+
+
 for i in range (0,var_n) :
     if i == 0 :
         var_a0 = mthd_get_coef(var_the_func , i , 'an')
@@ -92,15 +119,17 @@ for i in range (0,var_n) :
 
 for i in range (0,var_n):
     if i == 0 :
-        if var_a0 < 0.05 :
+        if abs(var_a0) < 0.05 :
             var_a0 = 0
     else :
-        if var_lst_an[i] < 0.001:
+        if abs(var_lst_an[i]) < 0.00001:
             var_lst_an[i] = 0
-        if var_lst_bn[i] < 0.001:
+        if abs(var_lst_bn[i]) < 0.00001:
             var_lst_bn[i] = 0
 
-
+print('var_a0 :' , var_a0)
+print('var_lst_an :' , var_lst_an)
+print('var_lst_bn :' , var_lst_bn)
 var_f_of_x = mthd_output(var_n , var_a0 , var_lst_an , var_lst_bn , var_axis_X)
 
 
@@ -108,7 +137,6 @@ var_f_of_x = mthd_output(var_n , var_a0 , var_lst_an , var_lst_bn , var_axis_X)
 
 array=numpy.array
 for i in range(1,len(var_lst_f_of_X)) :
-    Axes1.cla()
     mth_plot_graph (var_axis_X , var_the_func(var_axis_X) , var_lst_f_of_X[i] , i)
     pplt.pause(.01)
 
