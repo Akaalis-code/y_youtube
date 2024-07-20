5923d5181e22034fe73105d75bc2abe04f57118e
Akaalis-code <akaalis.y@gmail.com>
2024-07-20 23:23:09 +0530

    240720

---
+++ y_interview_questions/y_sql/y_delete_duplicates/y_delete_duplicate.sql
@@ -0,0 +1,29 @@
+
+CREATE TABLE y_tbl_customers_dup_info (name varchar(30) , address varchar(30), age int);
+
+
+
+
+INSERT INTO y_tbl_customers_dup_info 
+values   ('sai' , 'chennai' , 30),
+         ('sai' , 'chennai' , 30),
+    		 ('naveen' , 'delhi' , 20),
+    		 ('hari' , 'kochi' , 50),
+    		 ('hari' , 'kochi' , 50);
+
+
+select * from y_tbl_customers_dup_info;
+
+
+delete from y_tbl_customers_dup_info as tgt
+where EXISTS (select * from (
+                              select *,ROW_NUMBER() over(partition by name,address,age) as rn
+                              from y_tbl_customers_dup_info as ref
+                              where ref.name = tgt.name 
+                                and ref.address = tgt.address
+                                and ref.age = tgt.age
+                            ) as dt
+              where rn>1
+              );
+              
+select * from y_tbl_customers_dup_info;
