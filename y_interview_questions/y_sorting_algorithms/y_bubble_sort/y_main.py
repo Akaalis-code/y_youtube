27f63dc8c904f54e3e0dd798d43f6cb18995016a
Akaalis-code <akaalis.y@gmail.com>
2024-09-09 19:14:58 +0530

    202040909

--- y_interview_questions/y_sorting_algorithms/y_bubble_sort/y_main.py
+++ y_interview_questions/y_sorting_algorithms/y_bubble_sort/y_main.py
@@ -3,22 +3,38 @@ import numpy
 import random
 from y_plotting import *
 
-var_axis_X = numpy.linspace(0, 100, num=100)
-var_y = [random.randint(0,100) for _ in range(0,100) ]
+var_len = 30
+
+var_axis_X = numpy.linspace(0, var_len, num=var_len)
+var_y = [random.randint(0,var_len) for _ in range(0,var_len) ]
 
 
 sorting_y = var_y
 
+var_bar_colors = ['tab:blue' for _ in range(0,var_len)]
+var_traverse_color = ['tab:blue' for _ in range(0,var_len)]
 
 
-for i in range(0,100) :
+
+for i in range(0,var_len) :
+    if i == (var_len-1) :
+        var_bar_colors[(var_len-1)-i] = 'tab:green'
+        var_traverse_color[(var_len-1)-i] = 'tab:green'
+    
     for j in range(0,(len(var_y)-1)) :
+        #if var_traverse_color[j] != 'tab:green':
+        var_traverse_color[j] = var_bar_colors[j]
+        if j != (len(var_y)-2) :
+            var_traverse_color[j+1] = 'tab:red'
         if sorting_y[j]>sorting_y[j+1] :
             temp = sorting_y[j]
             sorting_y[j] = sorting_y[j+1]
             sorting_y[j+1] = temp
-        mth_plot(var_axis_X , sorting_y)
-        pplt.pause(.000001)
+        mth_plot(var_axis_X , sorting_y , var_traverse_color , var_bar_colors)
+        pplt.pause(.00000000000000000001)
+
+    var_bar_colors[(var_len-1)-i] = 'tab:green'
+    var_traverse_color[(var_len-1)-i] = 'tab:green'
 
 
 
--- y_interview_questions/y_sorting_algorithms/y_bubble_sort/y_plotting.py
+++ y_interview_questions/y_sorting_algorithms/y_bubble_sort/y_plotting.py
@@ -12,9 +12,11 @@ gs = var_fig.add_gridspec(1,1)
 Axes1 = var_fig.add_subplot(gs[0, :])
 
 
-def mth_plot(para_x , para_y):
+def mth_plot(para_x , para_y , para_bar_colors , var_rough= 'hi'):
     Axes1.cla()
-    Axes1.bar(para_x, para_y, color='#6bbc6b')
+    Axes1.bar(para_x, para_y, color=para_bar_colors)
+    #Axes1.text(0,19,para_bar_colors,fontsize =  8)
+    #Axes1.text(0,20,var_rough,fontsize =  8)
 
 
 
