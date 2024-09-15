56ff4c4842c88af3771ac28cf216db9a9fb61adb
Akaalis-code <akaalis.y@gmail.com>
2024-09-15 11:31:51 +0530

    20240915

---
+++ Y_python/y_python_variables.py
@@ -0,0 +1,86 @@
+# x = 123
+# y = x
+
+# print('Before : ',x,y)
+# y = 5
+# print('After : ',x,y)
+
+
+
+
+
+# x = '123'
+# y = x
+
+# print('Before : ',x,y)
+# y = '5'
+# print('After : ',x,y)
+
+
+
+
+
+# x = [1,1,1]
+# y = x
+
+# print('Before : ',x,y)
+# y[1] = 22
+# print('After : ',x,y)
+
+
+
+
+
+
+
+
+x = {'A':1 , 'B':2 , 'C':3}
+y = x
+
+print('Before : ',x,y)
+y['B'] = 22
+print('After : ',x,y)
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+
+# lst_1 = [ random.randint(0,1000) for _ in range(0,10) ]
+# lst_2 = lst_1
+
+# print(lst_1)
+# print(lst_2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+
+# lst_1 = [ random.randint(0,1000) for _ in range(0,10) ]
+# lst_2 = [ random.randint(0,1000) for _ in range(0,10) ]
+
+# print(lst_1)
+# print(lst_2)
+
+
+
--- y_interview_questions/y_pyspark/y_pyspark_basic/y_pyspark_basics.py
+++ y_interview_questions/y_pyspark/y_pyspark_basic/y_pyspark_basics.py
@@ -1,10 +1,16 @@
-// pip install pyspark 
+1 - Create sample data frames  
+2 - Show dataframes 
+3 - select only few columns
+4 - Alias column names
+5 - Filter Columns 
+6 - Joins of dataframes
+7 - Aggregate Functions
 
 from pyspark.sql import SparkSession
 
 ss=SparkSession.builder.getOrCreate()
 
-students_df = ss.createDataFrame( [(1,'rama','A') , (2,'raju','A') , (1,'rama','B')]  ,  ('ID','name','section') )
+students_df = ss.createDataFrame( [(1,'harish','A')  , (2,'venkata rama raju','A') , (3,'rajesh','B')]  ,  ('ID','name','section') )
 results_df  = ss.createDataFrame( [(1,'physics',94),(2,'maths',95),(1,'maths',90)]  ,  ('ID','subject','marks_scored') )
 
 students_df.show()
