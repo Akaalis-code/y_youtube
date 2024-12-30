f8386e58c81f26320a0390fdbef9861da121cee9
akaalis-code <akaalis.y@gmail.com>
2024-12-30 15:08:55 +0000

    241230_2038

--- Y_machine_learning/Y_ollama/y_main_using_pyttsx3.py
+++ Y_machine_learning/Y_ollama/y_main_using_pyttsx3.py
@@ -86,23 +86,34 @@
 #     return response
 
 
-import speech_recognition
-var_ear = speech_recognition.Recognizer()
-var_ear.energy_threshold = 1568 
 
-with speech_recognition.Microphone() as var_mic: 
-    var_ear.adjust_for_ambient_noise(var_mic,duration = 0.2)
-    while True:
-        try: 
-            print("listening...")
-            audio_data = var_ear.listen(var_mic) 
-            print("Recognizing...") 
-            print(audio_data)
-            text = var_ear.recognize_google(audio_data) 
-            print(f"Recognized text: {text}") 
-        except Exception as e: 
-            print(f" error; {e}")
-            var_ear = speech_recognition.Recognizer()
+######################## To recognize audio input devices ## start #########################################################
+
+# import speech_recognition as sr
+# for index, name in enumerate(sr.Microphone.list_microphone_names()):
+#     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
+######################## To recognize audio input devices ## end #########################################################
+
+
+# import speech_recognition
+# var_ear = speech_recognition.Recognizer()
+# var_ear.energy_threshold = 1568 
+
+# with speech_recognition.Microphone(device_index=2) as var_mic: 
+#     var_ear.adjust_for_ambient_noise(var_mic,duration = 0.2)
+#     while True:
+#         try: 
+#             print("listening...")
+#             audio_data = var_ear.listen(var_mic) 
+            
+#             print("Recognizing...") 
+#             print(audio_data)
+#             text = var_ear.recognize_google(audio_data) 
+#             print(f"Recognized text: {text}") 
+#         except Exception as e: 
+#             print(f" error; {e}")
+#             var_ear = speech_recognition.Recognizer()
 
 ## Solution  at the end by Luban6887 
     ## ref :- https://github.com/Uberi/speech_recognition/issues/100
@@ -114,3 +125,5 @@ with speech_recognition.Microphone() as var_mic:
 #     #print(var_resp['message']['content'])
 #     var_mouth.say(str(var_resp['message']['content']))
 #     var_mouth.runAndWait()
+
+
